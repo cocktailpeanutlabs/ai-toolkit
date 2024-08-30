1502bcd5a8e5a2fb3130c36f9514798fc2a99e9a
cocktailpeanut <cocktailpeanuts@proton.me>
2024-08-30 19:03:27 -0400

    update

--- install.js
+++ install.js
@@ -9,6 +9,15 @@ module.exports = {
         ]
       }
     },
+    {
+      method: "shell.run",
+      params: {
+        path: "app",
+        message: [
+          "git submodule update --init --recursive"
+        ]
+      }
+    },
     // Delete this step if your project does not use torch
     {
       method: "script.start",
@@ -28,7 +37,6 @@ module.exports = {
         venv: "env",                // Edit this to customize the venv folder path
         path: "app",                // Edit this to customize the path to start the shell from
         message: [
-          "pip install gradio devicetorch",
           "pip install -r requirements.txt"
         ]
       }
