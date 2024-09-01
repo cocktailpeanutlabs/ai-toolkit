70a14d92a9ea62d11796a1d5fc9df0b614f92ee1
cocktailpeanut <cocktailpeanuts@proton.me>
2024-09-01 06:35:54 -0400

    update

--- install.js
+++ install.js
@@ -5,7 +5,7 @@ module.exports = {
       method: "shell.run",
       params: {
         message: [
-          "git clone https://github.com/ostris/ai-toolkit app",
+          "git clone https://github.com/peanutcocktail/ai-toolkit app",
         ]
       }
     },
